f6d4a33a253336ce3c4be6030e0465870ac04b42
Mohdshoyab18 <135935405+Mohdshoyab18@users.noreply.github.com>
2023-06-22 22:43:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+   function redirectToURL(btnId){
+   	if(btnId=="button1")
+   		window.location.replace("https://mohdshoyab18.github.io/IMAGE-SLIDER/");
+   	else if(btnId=="button2")
+   		window.location.replace("https://mohdshoyab18.github.io/HUNGAMA-GAME/");
+   }
+</script>
